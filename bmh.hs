bccda66db15e86e3cb936ec80a77584c18c7f4e4
rmurai <rmurai270@gmail.com>
2021-06-21 21:10:37 +0900

    change to detect all occurences of pattern

--- bmh.hs
+++ bmh.hs
@@ -1,21 +1,19 @@
 import Data.List
 
-skip :: Char -> String -> Int --function for determining the number of charcharacters skipped in BMH algorithm
+skip :: Char -> String -> Int --determines the number of charcharacters skipped in BMH algorithm
 skip c s = if (c == last s) && (not $ elem c (init s))
            then length s
            else case elemIndex c (reverse $ init s) of
                 Just n -> n+1
                 Nothing -> length s
 
-bmh :: String -> String -> Maybe Int --implementation of BMH algorithm
+bmh :: String -> String -> [Int] --detects all occurences of a given pattern in a given text by BMH algortihm
 bmh text pat
  | tlgth >= plgth = let checked = take plgth text in
                         if  checked == pat
-                        then Just 1
+                        then 1 : map (+ plgth) (bmh (drop plgth text) pat)
                         else let skip_amount = skip (last checked) pat in
-                                case bmh (drop skip_amount text) pat of
-                                Just n -> Just $ n + skip_amount
-                                Nothing -> Nothing
- | otherwise = Nothing
+                                map (+ skip_amount) (bmh (drop skip_amount text) pat)
+ | otherwise = []
  where tlgth = length text
        plgth = length pat
